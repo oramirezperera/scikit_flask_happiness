b0ac94b570b42a729c7688660dde735ce79ea08c
oramirezperera <ramirez.orlando17@gmail.com>
2025-04-23 17:52:31 -0400

    Changes to server.py

--- server.py
+++ server.py
@@ -5,3 +5,9 @@ from flask import Flask
 from flask import jsonify
 
 app = Flask(__name__)
+
+if __name__ == '__main__':
+    
+    model = joblib.load('./models/best_model.pkl')
+
+    app.run(port=8080)
